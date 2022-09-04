08498d2a9e37a8b3f4f12cc79976ea10242ebace
Alan Araujo <alanaraujodev0@gmail.com>
2022-09-04 11:03:08 -0300

    Add exercicio 13 de Estrutura de Repetição

---
+++ 3-Estrutura-de-repetição/Exercicio-13.py
@@ -0,0 +1,5 @@
+print('POTÊNCIA')
+base = float(input('Digite o 1º número: '))
+expoente = float(input('Digite o 2º número: '))
+resultado = base ** expoente
+print(f'O resultado de {base} elevado a {expoente} é: {resultado}')

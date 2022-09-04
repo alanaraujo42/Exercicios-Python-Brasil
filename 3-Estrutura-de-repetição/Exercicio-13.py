7f38ebc59b7963dd8fc0be798346e4b2c68c6544
Alan Araujo <alanaraujodev0@gmail.com>
2022-09-04 11:24:20 -0300

    update exercicio 13, estrutura de repetição com while

--- 3-Estrutura-de-repetição/Exercicio-13.py
+++ 3-Estrutura-de-repetição/Exercicio-13.py
@@ -1,5 +1,9 @@
 print('POTÊNCIA')
 base = float(input('Digite o 1º número: '))
 expoente = float(input('Digite o 2º número: '))
-resultado = base ** expoente
+contador = int(2)
+resultado = base * base
+while contador <= expoente - 1:
+  resultado = resultado * base
+  contador = contador + 1
 print(f'O resultado de {base} elevado a {expoente} é: {resultado}')

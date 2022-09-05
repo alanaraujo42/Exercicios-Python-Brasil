567869c585e7ca82314a44dde18d9df0c099ce58
Alan Araujo <alanaraujodev0@gmail.com>
2022-09-05 17:12:21 -0300

    add exercício 21 de estrutura de repetição

---
+++ 3-Estrutura-de-repetição/Exercicio-21.py
@@ -0,0 +1,14 @@
+print('VERIFICAÇÃO DE NÚMERO PRIMO')
+numero = int(input('Digite um número inteiro: '))
+lista = list(range(2, numero))
+primo = int(0)
+for item in lista:
+  teste = numero % item
+  if teste == 0:
+    primo = primo + 1
+  else: 
+    primo = primo + 0
+if primo != 0 or numero == 2:
+  print('Não é um número primo!')
+else:
+  print('É um número primo!')

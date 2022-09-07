29afc058c56cf71f36980964d9227de836803d48
Alan Araujo <alanaraujodev0@gmail.com>
2022-09-07 10:48:31 -0300

    Corrigindo exercício 22 de estrutura de repetiação

--- 3-Estrutura-de-repetição/Exercicio-22.py
+++ 3-Estrutura-de-repetição/Exercicio-22.py
@@ -1,15 +1,16 @@
 print('VERIFICAÇÃO DE NÚMERO PRIMO V.2')
 numero = int(input('Digite um número inteiro: '))
-lista = list(range(2, numero))
+lista = list(range(1, numero + 1))
 primo = int(0)
 listadivisivel = [1]
 for item in lista:
   teste = numero % item
-  if teste == 0:
+  if teste == 0 and item != numero and item != 1:
     primo = primo + 1
     listadivisivel.append(item)
-listadivisivel.append(numero)
-if primo != 0 or numero == 2:
+if numero != 1:
+  listadivisivel.append(numero)
+if primo != 0 or numero == 1:
   print('Não é um número primo!')
   print(f'Ele é divisível por {listadivisivel}')
 else:

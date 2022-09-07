b3cea2ca04304a9de178f41c675d8d20e686c6e1
Alan Araujo <alanaraujodev0@gmail.com>
2022-09-07 10:33:20 -0300

    add update exercício 21, consertando pequeno erro

--- 3-Estrutura-de-repetição/Exercicio-21.py
+++ 3-Estrutura-de-repetição/Exercicio-21.py
@@ -1,12 +1,12 @@
 print('VERIFICAÇÃO DE NÚMERO PRIMO')
 numero = int(input('Digite um número inteiro: '))
-lista = list(range(2, numero))
+lista = list(range(1, numero + 1))
 primo = int(0)
 for item in lista:
   teste = numero % item
-  if teste == 0:
+  if teste == 0 and item != numero and item != 1:
     primo = primo + 1
-if primo != 0 or numero == 2:
+if primo != 0 or numero == 1:
   print('Não é um número primo!')
 else:
   print('É um número primo!')

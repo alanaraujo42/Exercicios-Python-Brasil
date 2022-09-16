3809fa91bd39d8947bfb1bcc34ce619da0b28ab1
Alan Araujo <alanaraujodev0@gmail.com>
2022-09-16 10:37:00 -0300

    add exercício 51 de estrutura de repetição

---
+++ 3-Estrutura-de-repetição/Exercício-51.py
@@ -0,0 +1,14 @@
+print('N TERMOS DE UMA SÉRIE')
+numero = int(input('Digite um número inteiro: '))
+n = int(1)
+m = int(1)
+soma = n / m
+print('\nS = 1/1', end='')
+while n != numero:
+  n += 1
+  m += 2
+  print(f' + {n}/{m}', end='')
+  soma += n / m
+print(f'\nA soma da série é: {soma:.2f}')
+
+# Questão repetida com a do exercício 49

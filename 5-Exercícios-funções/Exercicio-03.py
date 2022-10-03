3fb550fa880c81c295995cac2eb06b331678f737
Alan Araujo <alanaraujodev0@gmail.com>
2022-10-03 09:09:53 -0300

    add exercício 03 de exercícios de função

---
+++ 5-Exercícios-funções/Exercicio-03.py
@@ -0,0 +1,7 @@
+def soma(item1, item2, item3):
+  resultado = item1 + item2 + item3
+  print(resultado)
+n1 = float(input('Digite um número: '))
+n2 = float(input('Digite o segundo número: '))
+n3 = float(input('Digite o tercerio número: '))
+soma(n1, n2, n3)
